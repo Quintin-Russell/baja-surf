c1ce5bc50ff6edd589e5d8e1e47b648894c4c2ff
quintin russell <quintinrussell@corporatetools.com>
2022-11-23 16:34:35 -0800

    working on adding map

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,23 +1,27 @@
 <template>
   <div>
     <h1></h1>
-    <GmapMap>
-    </GmapMap>
+    <div ref="mapDiv"></div>
   </div>
 </template>
 
 <script lang="ts">
 import { Loader } from '@googlemaps/js-api-loader'
-import { onMounted } from 'vue'
+import { onMounted, ref } from 'vue'
 const GOOGLE_MAP_API_KEY = 'AIzaSyC9fgIeNyg1EfSqwkuwzL--2lmYhIQE8_Y'
 //video: https://www.youtube.com/watch?v=m4ad3eEFhAo
 
 export default {
     setup() {
         const loader = new Loader({ apiKey: GOOGLE_MAP_API_KEY })
+        const mapDiv = ref(null)
         onMounted(async () => {
             loader.load()
+            new google.maps.Map(mapDiv.value, {
+              zoom: 7
+            })
         })
+        return { mapDiv }
     }
 }
 </script>

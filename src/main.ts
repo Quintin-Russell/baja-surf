71f612a7957a6a1abdbf21e9b6e8799b3b9e6f25
quintin russell <quintinrussell@corporatetools.com>
2022-12-01 16:50:50 -0800

    removed import statement

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import App from "./App.vue"
 import router from "./router"
 import store from "./store"
 import { BootstrapVue } from "bootstrap-vue"
-// import * as VueGoogleMaps from "vue2-google-maps"
 
 createApp(App)
   .use(store)
